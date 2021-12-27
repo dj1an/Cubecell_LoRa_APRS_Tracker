bca6d27644e2a5d69b4ac34a79c4c5676eb9545a
dj1an <dj1an@darc.de>
2021-12-27 17:49:50 +0100

    readme changes

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Cubecell GPS LoRa APRS Tracker
 
-This is a fork of @peterus LoRa APRS ESP32 Tracker ported for the Heltec Cubecell GPS ASR650x CPU
+This Project is a fork of [@peterus LoRa APRS ESP32 Tracker](https://github.com/lora-aprs/LoRa_APRS_Tracker) ported for the Heltec Cubecell GPS ASR650x CPU
 
-
-
-![TTGO T-Beam](pics/Tracker.png)
+The code has many parts of the [CubeCell-GPS Helium Mapper](https://github.com/hkicko/CubeCell-GPS-Helium-Mapper)
 
 ## Supported boards
 
@@ -12,14 +10,20 @@ At this moment, only the Heltec CubeCell GPS is supported:
 
 * Heltec CubeCell GPS-6502 ([HTCC-AB02S](https://heltec.org/project/htcc-ab02s/)) 
 
-This board cost around 25-30 Euros, a little bit cheaper and smaller than the TTGO T-Beam.
+This board cost around 25-30 Euros, a little bit cheaper and smaller than the widely used TTGO T-Beam.
 Keep in mind: you need a 433MHz version!
 
+## Project State
+
+First Goal was the Port to the ASR650x platform, keeping the great Tracking and SmartBeacon Functions from Peters Project.
+
+This Firmware is still in a very early and not well-tested State.
+Expect Bugs and Functions not working.
+
+Please feel free to open an issue if you found Bugs.
+
+
 ## Compiling and configuration
-
-**There is a german [quick start](https://www.lora-aprs.info/docs/LoRa_APRS_iGate/quick-start-guide/) page! Take a look ;)**
-
-**There is a french [quick start](http://www.f5kmy.fr/spip.php?article509) page! Take a look ;)**
 
 ### How to compile
 
@@ -28,15 +32,9 @@ The best success is to use PlatformIO (and it is the only platform where I can s
 * Go to [PlatformIO](https://platformio.org/) download and install the IDE. 
 * If installed open the IDE, go to the left side and klick on 'extensions' then search for 'PatformIO' and install.
 * When installed click 'the ant head' on the left and choose import the project on the right.
-* Just open the folder and you can compile the Firmware.
+* Just open the folder and you can compile and upload the Firmware.
 
 ### Configuration
 
 * You can find all necessary settings to change for your configuration in **src/config.h**.
 
-## Project State
-
-This Firmware is still in Beta State.
-First Goal was the Port to the ASR650x platform, keeping the great Tracking and SmartBeacon Functions from Peters Project.
-
-Please feel free to open an issue if you found Bugs.
---
+++ src/config.h
@@ -0,0 +1,86 @@
+// Cubecell GPS - LoRa APRS Config
+// 
+// you can create three Profiles which can be changed via Menu
+//
+// Symbol Lookup Table:
+// https://blog.thelifeofkenneth.com/2017/01/aprs-symbol-look-up-table.html
+
+#define DEFAULT_PROFILE 0 // Profile to use after Reboot
+
+// Profile 0 - Car Symbol
+#define BEACON_SYMBOL ">"
+#define BEACON_OVERLAY "/"
+#define BEACON_MESSAGE  "Cubecell LoRa Tracker"
+#define SB_ACTIVE   true // smart Beacon
+#define BEACON_TIMEOUT  60  // fixed Beacon interval in s if Smartbeacon inactive
+#define CALLSIGN    "DJ1AN-7"   // your callsign
+
+// Profile 1 - Runner Symbol
+#define P1_BEACON_SYMBOL "["
+#define P1_BEACON_OVERLAY "/"
+#define P1_BEACON_MESSAGE  "Cubecell LoRa Tracker2"
+#define P1_SB_ACTIVE   true // smart Beacon
+#define P1_BEACON_TIMEOUT  60  // fixed Beacon interval in s if Smartbeacon inactive
+#define P1_CALLSIGN    "DJ1AN-8"   // your callsign
+
+// Profile 2 - Balloon
+#define P2_BEACON_SYMBOL "O"
+#define P2_BEACON_OVERLAY "/"
+#define P2_BEACON_MESSAGE  "Cubecell LoRa powered Balloon"
+#define P2_SB_ACTIVE   false // smart Beacon
+#define P2_BEACON_TIMEOUT  30  // fixed Beacon interval in s if Smartbeacon inactive
+#define P2_CALLSIGN    "DJ1AN-12"   // your callsign
+
+
+
+
+// Global Setting for All Profiles
+// 
+
+//#ifndef Lora_RGB
+#define LORA_RGB true  //activate Neopixel RGB LED
+//#endif
+#define MENU_IDLE_TIMEOUT     7000     // Auto exit the menu if no button pressed in this amount of ms
+#define DISPLAY_IDLE_TIMEOUT     15000     // Auto Shut OFF Display after re-activating in Shut-Off Mode
+#define VBAT_CORRECTION       1.004     // Edit this for calibrating your battery voltage
+
+#define RF_FREQUENCY                                433775000 // Hz
+#define TX_OUTPUT_POWER                             20        // dBm
+#define LORA_BANDWIDTH                              0         // [0: 125 kHz,
+                                                              //  1: 250 kHz,
+                                                              //  2: 500 kHz,
+                                                              //  3: Reserved]
+#define LORA_SPREADING_FACTOR                       12        // [SF7..SF12]
+#define LORA_CODINGRATE                             1         // [1: 4/5,
+                                                              //  2: 4/6,
+                                                              //  3: 4/7,
+                                                              //  4: 4/8]
+#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT                         0         // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON                  false
+#define LORA_IQ_INVERSION_ON                        false
+
+#define RX_TIMEOUT_VALUE                            1000
+#define BUFFER_SIZE                                 200 // Define the payload size here (max 255?)
+
+#define DEBUG false // Enable/Disable debug output over the serial console
+#define EXT_GPS_DATA    false   // Feed external GPS NMEA Data via Serial
+
+
+#define PTT_ACTIVE  false // ptt output
+#define PTT_IO_PIN  0   // ptt pin
+#define PTT_REVERSE false // invert ptt pin
+#define PTT_START_DELAY 150 // delay RF for x ms
+#define PTT_END_DELAY 0 // delay after sending
+
+#define SB_TURN_MIN 25 // minimum Turn Angle in degrees
+#define SB_SLOW_RATE 300 // every n seconds at SLOW_SPEED
+#define SB_SLOW_SPEED   10 // kmh
+#define SB_FAST_RATE    60 // every n seconds at FAST_SPEED
+#define SB_FAST_SPEED 100 // kmh
+#define SB_MIN_TX_DIST  100 // minimum Distance between Beacons
+#define SB_MIN_BCN  5 // seconds minimum Smart Beacon interval
+
+#define BEACON_BUTTON_TX    true
+#define ENHANCE_PRECISION   true
+
